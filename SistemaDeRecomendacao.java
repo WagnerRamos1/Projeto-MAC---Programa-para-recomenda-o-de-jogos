39a7d323339cab71e3fb7d3a739734481c33f32a
Samuel-Junior0 <samialcantara1958@gmail.com>
2025-11-06 09:13:44 -0300

    Update SistemaDeRecomendacao.java

--- SistemaDeRecomendacao.java
+++ SistemaDeRecomendacao.java
@@ -2,30 +2,33 @@ import java.util.Scanner;
 public class SistemaDeRecomendacao {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
+        System.out.println("====Sistema de Recomendação de Jogos====");
+        
+        // No Raiz
         No raiz = new No("Qual plataforma você prefere?");
 
-        // No PC
+        // No PC - 
         No pc = new No("Você prefere um estilo realista? ");
-        No pcRealista = new No("Você prefere jogar sozinho?");
-        No pcSolo = new No("Você prefere jogos com foco em modo história?");
-        No pcHistoria = new No("Gosta de tomar decisões que afetam a narrativa?");
+        No pcRealista = new No("Você prefere jogar sozinho? ");
+        No pcSolo = new No("Você prefere jogos com foco em modo história? ");
+        No pcHistoria = new No("Gosta de tomar decisões que afetam a narrativa? ");
         pcHistoria.sim = new No("The Witcher 3 ou Disco Elysium", true);
         pcHistoria.nao = new No("Tomb Raider ou Firewatch", true);
 
-        No pcAcao = new No("Prefere jogos de Ação em Mundo aberto?");
+        No pcAcao = new No("Prefere jogos de Ação em Mundo aberto? ");
         pcAcao.sim = new No("Skyrim ou GTA V", true);
         pcAcao.nao = new No("Doom Eternal ou COntrol", true);
         
         pcSolo.sim = pcHistoria;
         pcSolo.nao = pcAcao;
         
-        No pcMulti = new No("Prefere Jogos de Cooperação?");
-        No pcCooperativo = new No("Gosta de jogos casuais?");
+        No pcMulti = new No("Prefere Jogos de Cooperação? ");
+        No pcCooperativo = new No("Gosta de jogos casuais? ");
         pcCooperativo.sim = new No("Overcooked ou Stardew Valley", true);
         pcCooperativo.nao = new No("*Payday 2 ou Deep Rock Galactic", true);
 
-        No pcCompetitivo = new No("Gosta de jogos de tiro?");
-        pcCompetitivo.sim = new No("Play Unknown's Battleground (PUBG) ou Counter Strike 2 (CS 2)");
+        No pcCompetitivo = new No("Gosta de jogos de tiro? ");
+        pcCompetitivo.sim = new No("Play Unknown's Battleground (PUBG) ou Counter Strike 2 (CS 2)", true);
         pcCompetitivo.nao = new No("StarCraft II ou Age of Empires IV", true);
 
         pcMulti.sim = pcCooperativo;
@@ -34,25 +37,25 @@ public class SistemaDeRecomendacao {
         pcRealista.sim = pcSolo;
         pcRealista.nao = pcMulti;
         
-        No pcEstilizado = new No("Você prefere jogar sozinho?");
-        No pcSolo_Est = new No("Gosta de jogos modo História?");
-        No pcHistoria_Est = new No("Gosta de tomar decisões que afetam a história?");
+        No pcEstilizado = new No("Você prefere jogar sozinho? ");
+        No pcSolo_Est = new No("Gosta de jogos modo História? ");
+        No pcHistoria_Est = new No("Gosta de tomar decisões que afetam a história? ");
         pcHistoria_Est.sim = new No("Undertale ou Life is Strange", true);
         pcHistoria_Est.nao = new No("Ori and the Blind Forest ou Hollow Knight", true);
 
-        No pcAcao_Est = new No("Você gostas de jogos de ação lineares?");
+        No pcAcao_Est = new No("Você gostas de jogos de ação lineares? ");
         pcAcao_Est.sim = new No("Celeste ou Dead Cells", true);
         pcAcao_Est.nao = new No("Minecraft ou Terraria", true);
 
         pcSolo_Est.sim = pcHistoria_Est;
         pcSolo_Est.nao = pcAcao_Est;
 
-        No pcMulti_Est = new No("Gosta de jogos competitivos?");
-        No pcCompetitivo_Est = new No("Prefere jogos de estratégia?");
+        No pcMulti_Est = new No("Gosta de jogos competitivos? ");
+        No pcCompetitivo_Est = new No("Prefere jogos de estratégia? ");
         pcCompetitivo_Est.sim = new No("Civilization VI e League of Legends (LOL)", true);
         pcCompetitivo_Est.nao = new No("Valorant ou Fortnite", true);
 
-        No pcCooperativo_Est = new No("Prefere jogos desafiadores?");
+        No pcCooperativo_Est = new No("Prefere jogos desafiadores? ");
         pcCooperativo_Est.sim = new No("Cuphead ou Don't Starve Together", true);
         pcCooperativo_Est.nao = new No("Lovers in a Dangerous Spacetime ou Moving Out", true);
 
@@ -64,12 +67,12 @@ public class SistemaDeRecomendacao {
 
         pc.sim = pcRealista;
         pc.nao = pcEstilizado;
-        //Fim do No Pc
+        // Fim do No Pc
 
-        //No Console
+        // No Console
         No console = new No("Você prefere um visual estilizado? ");
-        No consoleEstilizado = new No("Você prefere jogar com outras pessoas?");
-        No consoleMulti = new No("Você prefere jogos competitivos?");
+        No consoleEstilizado = new No("Você prefere jogar com outras pessoas? ");
+        No consoleMulti = new No("Você prefere jogos competitivos? ");
         No consoleCompetitivo = new No("Você gosta de jogos de luta? ");
         consoleCompetitivo.sim = new No("Tekken 6 ou Street Fighter", true);
         consoleCompetitivo.nao = new No("Fortnite ou Splatoon", true);
@@ -121,14 +124,11 @@ public class SistemaDeRecomendacao {
 
         console.sim = consoleEstilizado;
         console.nao = consoleRealista;
-        // fim do Console
-
-        raiz.sim = pc;
-        raiz.nao = console;
-
-    
-        Funcoes.Recomendar(raiz, pc, console, ler);
         
+        // Fim do No console
+
+        Funcoes.Recomendar(raiz, pc, console, ler);
+        ler.close();
         
     }
 }
